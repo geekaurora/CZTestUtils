fee1c04183de55cf07ce699e25bc6d27e308f49b
Cheng Zhang <showt2me@gmail.com>
2021-03-22 15:26:54 +0900

    Minor update.

--- Sources/CZTestUtils/CZTestUtils.swift
+++ Sources/CZTestUtils/CZTestUtils.swift
@@ -7,20 +7,28 @@ public class CZTestUtils {
     static let waitExpectationDescription = "waitForInterval"
   }
   
-  /// Returns tuple of`WaitExpectation` closure and XCTestExpectation.
-  ///
-  /// ### Usage
-  /// ```
-  /// let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(20, testCase: self)
-  /// waitExpectation()
-  /// ```
-  /// Then call `expectation.fulfill()` to fulfill the expectation.
-  ///
-  /// - Parameters:
-  ///   - interval: the internal of timeout
-  ///   - testCase: the testCase which contains expectation
-  /// - Returns: Tuple of`WaitExpectation` closure and XCTestExpectation.
-  ///            Should call `WaitExpectation` closure to make`testCase` waiting before timeout of `interval`.
+  /**
+   Returns tuple of`WaitExpectation` closure and XCTestExpectation.
+   
+   ### Usage
+   ```
+   // 1. Intialize the async expectation.
+   let (waitForExpectatation, expectation) = CZTestUtils.waitWithInterval(1, testCase: self)
+
+   // 2. Wait for the expectatation.
+   waitForExpectatation()
+   
+   // 3. Fulfill the expectatation.
+   expectation.fulfill()
+   ```
+   
+   - Parameters:
+     - interval: the internal of timeout
+     - testCase: the testCase which contains expectation
+   - Returns: Tuple of`WaitExpectation` closure and XCTestExpectation.
+              Should call `WaitExpectation` closure to make`testCase` waiting before timeout of `interval`.
+
+   */
   public static func waitWithInterval(_ interval: TimeInterval,
                                       testCase: XCTestCase) -> (WaitExpectation, XCTestExpectation) {
     let expectation = XCTestExpectation(description: Constant.waitExpectationDescription)
